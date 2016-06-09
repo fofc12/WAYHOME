20084db712e7f4f3726e0b180060885a0aaac54d
free2ing <haoyunmail@qq.com>
2016-06-09 14:42:49 +0800

    Release

--- proj.android/obj/local/armeabi/objs-debug/cocos2dx_internal_static/base/ccFPSImages.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs-debug/cocos2dx_internal_static/base/ccFPSImages.o: \
- D:\_MY\COCOS_PRJ\WayHome\proj.android\../cocos2d/cocos/./base/ccFPSImages.c \
- D:\_MY\COCOS_PRJ\WayHome\proj.android\../cocos2d/cocos/./base/ccFPSImages.h
-
-D:\_MY\COCOS_PRJ\WayHome\proj.android\../cocos2d/cocos/./base/ccFPSImages.h:
